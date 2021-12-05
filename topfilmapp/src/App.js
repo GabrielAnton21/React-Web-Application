5401243af14244faa6ae1ec8d2b8278e58b12b28
GabrielAnton21 <44251263+GabrielAnton21@users.noreply.github.com>
2021-12-05 17:48:31 -0500

    changed name and initial page

---
+++ topfilmapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      Culture's Peak
+    </div>
+  );
+}
+
+export default App;
